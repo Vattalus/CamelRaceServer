ded019205254e6084ed6902aa0b1aceb061d378e
Vattalus <VattalusAssets@gmail.com>
2017-06-20 12:15:48 +0300

    test

--- src/main_functions/GrantOasis.js
+++ src/main_functions/GrantOasis.js
@@ -1,3 +1,11 @@
 handlers.grantOasis = function (args, context) {
     var message = "this is just a test";
+
+    log.info(message);
+    var inputValue = null;
+    if (args && args.inputValue)
+        inputValue = args.inputValue;
+    log.debug("helloWorld:", { input: args.inputValue });
+
+    return { messageValue: message };
 }
