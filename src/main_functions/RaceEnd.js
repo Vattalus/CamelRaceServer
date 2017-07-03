f51796e5f2bcc5bdc87bb1168197a487d592c673
Vattalus <VattalusAssets@gmail.com>
2017-07-03 18:00:57 +0300

    test

--- src/main_functions/RaceEnd.js
+++ src/main_functions/RaceEnd.js
@@ -1,9 +1,5 @@
 handlers.raceEnd = function (args, context) {
 
-    log.debug("argument: ", args.endRaceReward);
-    log.debug("argument number: ", Number(args.endRaceReward));
-
-
     if (args != null && args.endRaceReward && Number(args.endRaceReward)) {
         addCurrency("SC", Number(args.endRaceReward));
     }
