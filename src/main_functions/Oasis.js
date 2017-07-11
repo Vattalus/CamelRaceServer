f5d6d9642de114436d4aa5f25e875bad8628b5d7
Vattalus <VattalusAssets@gmail.com>
2017-07-11 17:13:06 +0300

    test

--- src/main_functions/Oasis.js
+++ src/main_functions/Oasis.js
@@ -1,60 +1,3 @@
-function generateFailObj(mess, data) {
-    var retObj = {
-        Result: "Failed",
-        Message: mess,
-        Data: data
-    };
-    return retObj;
-}
-
-function generateErrObj(mess, data) {
-    var retObj = {
-        Result: "Error",
-        Message: mess,
-        Data: data
-    };
-    return retObj;
-}
-
-function loadTitleDataJson(key) {
-    var tData = server.GetTitleData(
-    {
-        PlayFabId: currentPlayerId,
-        Keys: [key]
-    }
-    );
-
-    if (tData == undefined || tData.Data == undefined || tData.Data[key] == undefined)
-        return null;
-
-    var tDataJSON = JSON.parse(tData.Data[key]);
-
-    if (tDataJSON == undefined)
-        return null;
-
-    return tDataJSON;
-}
-
-//get the current server time timestamp (seconds)
-function getServerTime() {
-    return Math.floor((new Date().getTime() / 1000));
-}
-
-//random int between min and max (both inclusive)
-function randomRange(min, max) {
-    return Math.round(Math.random() * (Number(max) - Number(min))) + Number(min);
-}
-
-//Add Virtual Currency
-function addCurrency(currCode, amount) {
-    server.AddUserVirtualCurrency(
-{
-    PlayFabId: currentPlayerId,
-    "VirtualCurrency": currCode,
-    "Amount": amount
-});
-}
-
 handlers.grantOasis = function (args, context) {
 
     //first of all, let's load the oasis balancing variables
@@ -136,12 +79,4 @@ function getNextOasisTime(oasisWaitTime) {
     }
 
     return lastClaimedOasisTimestamp + Number(oasisWaitTime) * 3600;
-}
-handlers.raceEnd = function (args, context) {
-
-    if (args != null && args.endRaceReward && Number(args.endRaceReward)) {
-        addCurrency("SC", Number(args.endRaceReward));
-    }
-
-    return { Result: "OK" };
 }
