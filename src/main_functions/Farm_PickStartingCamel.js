1276cdd47140602575d2ffb3c06ae6a96d3cd0dc
Vattalus <VattalusAssets@gmail.com>
2017-07-25 19:46:42 +0300

    test

---
+++ src/main_functions/Farm_PickStartingCamel.js
@@ -0,0 +1,16 @@
+//Adds the first camel to the player's list of owned camels
+//
+//Arguments
+//arg.finishPosition - placement of player (0- first, 1-seconds etc)
+//arg.startQteOutcome - outcome of the start qte (0-perfect, 1-great, 2-good etc)
+//arg.finishSpeedFactor - speed factor when crossing finish line (0-top speed, 1-top speed+max boost speed bonus)
+handlers.pickStartingCamel = function (args, context) {
+    //first of all, we need to make sure that the player does not already own a camel (starting camel can only be picked once)
+    var camels = server.GetUserReadOnlyData(
+    {
+        PlayFabId: currentPlayerId,
+        Keys: ["Camels"]
+    });
+
+    log.debug(camels);
+}
--- unified.js
+++ unified.js
@@ -55,6 +55,22 @@ function addCurrency(currCode, amount) {
 });
 }
 
+//Adds the first camel to the player's list of owned camels
+//
+//Arguments
+//arg.finishPosition - placement of player (0- first, 1-seconds etc)
+//arg.startQteOutcome - outcome of the start qte (0-perfect, 1-great, 2-good etc)
+//arg.finishSpeedFactor - speed factor when crossing finish line (0-top speed, 1-top speed+max boost speed bonus)
+handlers.pickStartingCamel = function (args, context) {
+    //first of all, we need to make sure that the player does not already own a camel (starting camel can only be picked once)
+    var camels = server.GetUserReadOnlyData(
+    {
+        PlayFabId: currentPlayerId,
+        Keys: ["Camels"]
+    });
+
+    log.debug(camels);
+}
 handlers.grantOasis = function (args, context) {
 
     //first of all, let's load the oasis balancing variables
