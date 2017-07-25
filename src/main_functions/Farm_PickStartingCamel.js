f19fac91fb83634a86ac065524981e49b6573f3e
Vattalus <VattalusAssets@gmail.com>
2017-07-25 20:53:48 +0300

    test

--- src/main_functions/Farm_PickStartingCamel.js
+++ src/main_functions/Farm_PickStartingCamel.js
@@ -1,9 +1,11 @@
 //Adds the first camel to the player's list of owned camels
 //
 //Arguments
-//arg.finishPosition - placement of player (0- first, 1-seconds etc)
-//arg.startQteOutcome - outcome of the start qte (0-perfect, 1-great, 2-good etc)
-//arg.finishSpeedFactor - speed factor when crossing finish line (0-top speed, 1-top speed+max boost speed bonus)
+//args.camelName - name of the camel
+//args.baseAcc
+//args.baseSpeed
+//args.baseGallop
+//args.baseStamina
 handlers.pickStartingCamel = function (args, context) {
     //first of all, we need to make sure that the player does not already own a camel (starting camel can only be picked once)
     var camels = server.GetUserReadOnlyData(
@@ -16,9 +18,27 @@ handlers.pickStartingCamel = function (args, context) {
     var camelsJSON = JSON.parse("[]");
 
     if ((camels.Data.Camels != undefined && camels.Data.Camels != null))
-        var camelsJSON = JSON.parse(camels.Data.Camels.Value);
+        camelsJSON = JSON.parse(camels.Data.Camels.Value);
 
     //if the player already owns at least one camel, they cannot pick a starting camel again. So, return a fail object
     if (camelsJSON.length > 0 && (camelsJSON[0].name != undefined || camelsJSON[0].name != null))
         return generateFailObj("Player already owns a camel");
+
+    //so far, everything seems to be ok
+
+    camelsJSON = new Array();
+    camelsJSON.push({
+        "name": "CamelName",
+        "baseAcc": 5,
+        "baseSpeed": 8,
+        "baseGallop": 3,
+        "baseStamina": 2
+    });
+
+    //update the player's readonly data
+    server.UpdateUserReadOnlyData(
+    {
+        PlayFabId: currentPlayerId,
+        Data: { "Camels": camelsJSON, "SelectedCamel": 0 }
+    });
 }
